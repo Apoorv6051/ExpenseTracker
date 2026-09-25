51364a03ee58c32b6cc4c13681a3f920891ed8e9
Apoorv6051 <apoorvpal8853277@gmail.com>
2026-09-25 09:39:23 +0530

    creating main.java

--- src/main.java
+++ src/main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class main {
-}
+    public static void main(String[] args) {
+
+        Expence expense = new Expence(
+                250.0,
+                "food",
+                "luch",
+                LocalDate.now()
+        );
+        System.out.println("Amount: " + expense.getAmount());
+        System.out.println("Category: " + expense.getCategory());
+        System.out.println("Description: " + expense.getDescription());
+        System.out.println("Date: " + expense.getDate());
+
+     }
+   }
+
