546363c9bb6c868d00f59e9367bd555726c7d04e
Apoorv6051 <apoorvpal8853277@gmail.com>
2026-09-25 09:43:24 +0530

    changes in m-->M done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.time.LocalDate;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
 
-        Expence expense = new Expence(
+        Expence expense = new Expence( //creates one Expense object:
                 250.0,
                 "food",
                 "luch",
