4bf795a17902f20189dc79cca47f43ba04496c99
Apoorv6051 <apoorvpal8853277@gmail.com>
2026-09-25 10:04:46 +0530

    Add expense model and total calculation

--- src/Expense.java
+++ src/Expense.java
@@ -1,12 +1,12 @@
 import java.time.LocalDate;
-public class Expence {
+public class Expense {
             // these are the data belong to Expences
     private Double amount; //We don't want other parts of the program directly changing the
     private String category;      //We don't want other parts of the program directly changing the
     private String Description; //We don't want other parts of the program directly changing the
     private LocalDate date; //We don't want other parts of the program directly changing the
 
-    public Expence(Double amount, String category, String Description , LocalDate date){  // this is constructor (It creates and initializes an Expense object.)
+    public Expense(Double amount, String category, String Description , LocalDate date){  // this is constructor (It creates and initializes an Expense object.)
 
         this.amount = amount;
         this.category = category;
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,45 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        Expence expense = new Expence( //creates one Expense object:
+        ArrayList<Expense> expenses = new ArrayList<>(); // to store multiple expences
+
+        expenses.add(new Expense(
                 250.0,
-                "food",
-                "luch",
+                "Food",
+                "Lunch",
                 LocalDate.now()
-        );
-        System.out.println("Amount: " + expense.getAmount());
-        System.out.println("Category: " + expense.getCategory());
-        System.out.println("Description: " + expense.getDescription());
-        System.out.println("Date: " + expense.getDate());
+        ));
+
+        expenses.add(new Expense(
+                500.0,
+                "Travel",
+                "Bus",
+                LocalDate.now()
+        ));
+
+        expenses.add(new Expense(
+                1000.0,
+                "Shopping",
+                "Shoes",
+                LocalDate.now()
+        ));
+
+        Double totalExpense =0.0;
+        for(Expense expense: expenses){
+            totalExpense +=expense.getAmount();
+        }
+        System.out.println("Total Expenses: " + totalExpense);
+
+//       for(Expense expense: expenses) {
+//           System.out.println("Amount: " + expense.getAmount());
+//           System.out.println("Category: " + expense.getCategory());
+//           System.out.println("Description: " + expense.getDescription());
+//           System.out.println("Date: " + expense.getDate());
+//           System.out.println("-----------------------------------------");
+//       }
 
      }
    }
